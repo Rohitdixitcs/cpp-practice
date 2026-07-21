54be9ea36d9b12012f3397131ba5d3899f414a25
rohit dixit <iamrohitdixit17@gmail.com>
2026-07-21 18:08:17 +0530

    Sum codes

---
+++ codeie1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"Enter a ";
+    cin>>a;
+    cout<<"Enter b ";
+    cin>>b;
+    cout<<"Sum of a and b is "<<a+b<<endl;
+    return 0;
+}
